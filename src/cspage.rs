7fd6d55e3295593c8d476fbad73eaf51dff1e6d4
ttrounce <toby.trounce111@gmail.com>
2022-08-18 22:58:33 +0100

    Simplified pages

--- src/cspage.rs
+++ src/cspage.rs
@@ -1,64 +1,64 @@
-use std::{io::Stdout, i32::MAX};
+use std::io::Stdout;
 
 use crossterm::event::KeyCode;
-use tui::{Frame, backend::CrosstermBackend, layout::{Layout, Direction, Constraint}, widgets::{ListState, ListItem, List, Block, Borders, Paragraph}, style::{Style, Modifier}};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Modifier, Style},
+    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
+    Frame,
+};
 
-use crate::state::{State, ProgramState};
+use crate::state::ProgramState;
 
-pub struct CharacterSheetState {
-    scroll_index: i32,
-    scroll_index_max: i32
-}
-
-impl State for CharacterSheetState {
-    fn new() -> Self {
-        CharacterSheetState { scroll_index: 0, scroll_index_max: 0  }
-    }
-}
-
-pub fn character_sheet_draw(local_state: &mut CharacterSheetState, global_state: &mut ProgramState, frame: &mut Frame<CrosstermBackend<Stdout>>) {
+pub fn character_sheet_draw(state: &mut ProgramState, frame: &mut Frame<CrosstermBackend<Stdout>>) {
     let cs_chunks = Layout::default()
         .direction(Direction::Horizontal)
         .margin(0)
-        .constraints(
-            [
-                Constraint::Percentage(20),
-                Constraint::Percentage(80)
-            ].as_ref()
-        )
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
         .split(frame.size());
-    
-    let mut scroll_list_state = ListState::default();
-    scroll_list_state.select(Some(local_state.scroll_index as usize));
 
-    let scroll_item_list = [ListItem::new("Item 0"), ListItem::new("Item 1"), ListItem::new("Item 2"), ListItem::new("Item 3")];
-    local_state.scroll_index_max = scroll_item_list.len() as i32;
-    
+    let mut scroll_list_state = ListState::default();
+    scroll_list_state.select(Some(state.scroll_index as usize));
+
+    let scroll_item_list = [
+        ListItem::new("Item 0"),
+        ListItem::new("Item 1"),
+        ListItem::new("Item 2"),
+        ListItem::new("Item 3"),
+    ];
+    state.scroll_index_max = scroll_item_list.len() as i32;
+
     let scroll_list = List::new(scroll_item_list)
         .block(Block::default().borders(Borders::ALL).title(" List "))
         .highlight_style(Style::default().add_modifier(Modifier::BOLD))
         .highlight_symbol("> ");
 
-    let cs_item_0 = Paragraph::new("Stuff all about Item 0!")
-        .block(Block::default().borders(Borders::ALL));
+    let cs_item_0 =
+        Paragraph::new("Stuff all about Item 0!").block(Block::default().borders(Borders::ALL));
 
     let cs_item_1 = Paragraph::new("Different stuff (it's all about Item 1)!")
         .block(Block::default().borders(Borders::ALL));
 
-    match local_state.scroll_index {
+    match state.scroll_index {
         0 => frame.render_widget(cs_item_0, cs_chunks[1]),
         1 => frame.render_widget(cs_item_1, cs_chunks[1]),
-        _ => ()
+        _ => (),
     }
 
     frame.render_stateful_widget(scroll_list, cs_chunks[0], &mut scroll_list_state);
 }
 
-pub fn character_sheet_key(local_state: &mut CharacterSheetState, state: &mut ProgramState, key: KeyCode) {
-    let scroll_index_max = local_state.scroll_index_max;
+pub fn character_sheet_key(state: &mut ProgramState, key: KeyCode) {
+    let scroll_index_max = state.scroll_index_max;
     match key {
-        KeyCode::Char('k') | KeyCode::Up => local_state.scroll_index = (((local_state.scroll_index - 1) % scroll_index_max) + scroll_index_max) %  scroll_index_max,
-        KeyCode::Char('j') | KeyCode::Down => local_state.scroll_index = (local_state.scroll_index + 1) % local_state.scroll_index_max,
-        _ => ()
+        KeyCode::Char('k') | KeyCode::Up => {
+            state.scroll_index = (((state.scroll_index - 1) % scroll_index_max) + scroll_index_max)
+                % scroll_index_max
+        }
+        KeyCode::Char('j') | KeyCode::Down => {
+            state.scroll_index = (state.scroll_index + 1) % state.scroll_index_max
+        }
+        _ => (),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 mod cspage;
 mod state;
 
-use cspage::{character_sheet_draw, character_sheet_key, CharacterSheetState};
 use crossterm::{
     event::{self, poll, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use state::{ProgramState, State};
+use cspage::{character_sheet_draw, character_sheet_key};
+use state::ProgramState;
 use std::{
+    hash::Hash,
     io::{self, stdout, Stdout},
-    time::Duration, hash::Hash, collections::{HashMap, hash_map},
+    time::Duration,
 };
 use tui::{backend::CrosstermBackend, Terminal};
 
 type Backend = CrosstermBackend<Stdout>;
 
-#[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
-pub enum Page
-{
-    None,
-    CharacterSheet
+#[derive(Eq, Hash, PartialEq)]
+pub enum Page {
+    CharacterSheet,
 }
 
 fn main() -> Result<(), io::Error> {
@@ -31,46 +30,22 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn start(terminal: &mut Terminal<Backend>) -> io::Result<()> {
-    let mut program_state = ProgramState {
-        objects: HashMap::new(),
-        current_page: Page::CharacterSheet,
-        current_key: None
-    };
-
-    let mut pages = HashMap::from([
-        (Page::CharacterSheet, CharacterSheetState::new())
-    ]);
+    let mut program_state = ProgramState::new();
 
     loop {
-
         if poll(Duration::from_secs(0))? {
             if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char('q') => break,
-                    _ => program_state.current_key = Some(key.code),
+                if key.code == KeyCode::Char('q') { return Ok(()) }
+                match program_state.current_page {
+                    Page::CharacterSheet => character_sheet_key(&mut program_state, key.code),
                 }
             }
-        } else {
-            program_state.current_key = None;
         }
 
-        let mut local_state = pages.get_mut(&program_state.current_page).unwrap();
-
-        terminal.draw(|f| {
-            match &mut program_state.current_page {
-                Page::CharacterSheet => character_sheet_draw(&mut local_state, &mut program_state, f),
-                _ => ()
-            }
+        terminal.draw(|f| match &mut program_state.current_page {
+            Page::CharacterSheet => character_sheet_draw(&mut program_state, f),
         })?;
-
-        if let Some(key) = program_state.current_key {
-            match program_state.current_page {
-                Page::CharacterSheet => character_sheet_key(&mut local_state, &mut program_state, key),
-                _ => ()
-            }
-        }
     }
-    Ok(())
 }
 
 fn create_terminal() -> Terminal<Backend> {
